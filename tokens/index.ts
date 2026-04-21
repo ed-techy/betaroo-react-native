00c2857b08bfe60b96087d7a9f280cfc61d33874
ed-techy <11414212+ed-techy@users.noreply.github.com>
2026-04-20 19:56:36 -0700

    remove the old tokens

--- tokens/index.ts
+++ tokens/index.ts
@@ -1,1 +1,8 @@
-export * from "./theme-tokens";
+export { colors } from "./primitives/colors";
+export { spacing } from "./primitives/spacing";
+export { radius } from "./primitives/radius";
+export { shadows } from "./primitives/shadows";
+export { fontFamily, typography } from "./primitives/typography";
+export { semantic } from "./semantic";
+export { darkTheme } from "./semantic/dark";
+export { lightTheme } from "./semantic/light";

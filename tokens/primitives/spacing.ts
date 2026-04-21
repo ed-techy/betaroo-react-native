f33ce178633bd55df4332a88ce3dff00f0e4c762
ed-techy <11414212+ed-techy@users.noreply.github.com>
2026-04-20 19:54:58 -0700

    refactor spacing

---
+++ tokens/primitives/spacing.ts
@@ -0,0 +1,17 @@
+export const spacing = {
+  0: 0,
+  2: 2,
+  4: 4,
+  6: 6,
+  8: 8,
+  10: 10,
+  12: 12,
+  14: 14,
+  16: 16,
+  24: 24,
+  32: 32,
+  40: 40,
+  48: 48,
+} as const;
+
+export type Spacing = typeof spacing;
